6eef1a1d8e6d536b575807b7429ba21ded7c178a
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-03-29 20:33:50 +0200

    FEAT: add route to homepage

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const indexRoutes = require('./routes/index.routes');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use('/', indexRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening in http://localhost:${PORT}`);
