b5795e8df7a40231fdb0b39d608189c28f9c41f8
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-01 12:35:52 +0200

    FEAT: add error management to index

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ const app = express();
 
 app.use('/', indexRoutes);
 
+
+app.use('*', (req, res, next) => {
+    const error = new Error('Route not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.status || 500).json(error.message || "Unexpected error");
+});
+
 app.listen(PORT, () => {
     console.log(`Server listening in http://localhost:${PORT}`);
 });
