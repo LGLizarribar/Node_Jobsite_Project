ab69cf22f621608f4c8f2da0b1d6699e1bcff3a8
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-01 12:59:23 +0200

    FEAT: add job creation

--- routes/jobs.routes.js
+++ routes/jobs.routes.js
@@ -13,4 +13,20 @@ router.get('/', async (req, res, next) => {
     }
 });
 
+
+router.post('/add-offer', async (req, res, next) => {
+    try {
+        const { position, company, description, contactEmail, location } = req.body;
+
+        const newJobOffer = new JobOffer({ position, company, description, contactEmail, location });
+
+        const savedJobOffer = await newJobOffer.save();
+
+        return res.status(201).json(savedJobOffer);
+
+    } catch(error) {
+        next(error);
+    }
+});
+
 module.exports = router;
