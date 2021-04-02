4e1fd7bf0db9cfd9515525f64bbed6bdefcc7a38
Laura Garcia Lizarribar <laura.garcia.lizarribar@gmail.com>
2021-04-02 19:02:55 +0200

    FEAT: add offer view

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const user = req.user;
     return res.render('index', {
         title: 'Bienvenido a JobFinder',
         content: "Si estás buscando tu trabajo soñado, ¡estás en el lugar adecuado!",
